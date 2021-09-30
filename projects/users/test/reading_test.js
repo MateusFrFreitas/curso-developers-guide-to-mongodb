52e8351e6af75f4999dc95d8a902d731fa39fda2
Mateus Franchini de Freitas <contato.mateusfr@outlook.com>
2021-09-29 21:16:12 -0300

    Pagination

--- projects/users/test/reading_test.js
+++ projects/users/test/reading_test.js
@@ -2,12 +2,18 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users out of the database", () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach(async () => {
+    alex = new User({ name: "Alex" });
     joe = new User({ name: "Joe" });
+    maria = new User({ name: "Maria" });
+    zach = new User({ name: "Zach" });
 
+    await alex.save();
     await joe.save();
+    await maria.save();
+    await zach.save();
   });
 
   it("finds all users with a name of joe", async () => {
@@ -20,5 +26,12 @@ describe("Reading users out of the database", () => {
     const user = await User.findOne({ _id: joe._id });
 
     assert(user.name === "Joe");
+  });
+
+  it("can skip and limit the result set", async () => {
+    const users = await User.find({}).sort({ name: 1 }).skip(1).limit(2);
+    assert(users.length === 2);
+    assert(users[0].name === "Joe");
+    assert(users[1].name === "Maria");
   });
 });
